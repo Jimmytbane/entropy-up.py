533c7119fa5d9d6f616a5a22cd2e9a97e4de0108
jimmybot <jimmybot@tuta.io>
2017-05-31 07:33:26 +0000

    Update 'entropy-up.sh'

--- entropy-up.sh
+++ entropy-up.sh
@@ -2,3 +2,8 @@
 system "clear"
 puts "Current entropy"
 system "cat /proc/sys/kernel/random/entropy_avail"
+puts nil
+puts "Installing haveged and rng-tools"
+system "sudo su"
+system "apt install haveged rng-tools"
+system "exit"
