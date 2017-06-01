1cfa3a7b11d0d49e945bc487e020bb9949842970
jimmybot <jimmybot@tuta.io>
2017-06-01 12:06:07 +0000

    Update 'entropy-up.rb'

--- entropy-up.rb
+++ entropy-up.rb
@@ -7,6 +7,9 @@ puts "Please enter any character and enter"
 input = gets.chomp
 puts "Installing haveged and rng-tools"
 system "sudo apt install haveged rng-tools"
+puts "If the syntax was incorrect, or another error occured, this is to give you a chance to see it."
+puts "Please enter any character and enter"
+input = gets.chomp
 system "clear"
 puts "haveged and rng-tools installed"
 puts "Entropy should be going up"
