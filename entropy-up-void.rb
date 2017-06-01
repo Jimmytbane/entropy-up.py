81e0ca7833c5e28412b7881b35bad1af57830fda
jimmybot <jimmybot@tuta.io>
2017-06-01 04:56:56 +0000

    Add 'entropy-up-void.rb'

---
+++ entropy-up-void.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+system "clear"
+puts "Current entropy"
+system "cat /proc/sys/kernel/random/entropy_avail"
+puts "Installing haveged and rng-tools"
+system "sudo apt install haveged rng-tools"
+system "clear"
+puts "haveged and rng-tools installed"
+puts "Entropy should be going up"
+puts "Check out your entropy now"
+puts "Current entropy"
+system "cat /proc/sys/kernel/random/entropy_avail"
