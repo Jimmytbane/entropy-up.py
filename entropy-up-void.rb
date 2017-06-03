845c082a8c7af299095f0b998a814b3da65b5e73
jimmybot <jimmybot@tuta.io>
2017-06-03 15:04:53 +0000

    Update 'entropy-up-void.rb'

--- entropy-up-void.rb
+++ entropy-up-void.rb
@@ -1,3 +1,18 @@
+#    Copyright (C) <2017>  <jimmybot@tuta.io (jimmybot@riseup.net) PGP key: "374C C0C2 9153 8F0E F877  D8F6 B0A8 8E2F DECC DEE1">
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #!/usr/bin/ruby
 system "clear"
 puts "Current entropy"
