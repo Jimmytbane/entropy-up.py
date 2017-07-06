9324d84a241018875cc2cbdf59d10c303c3921e2
JimmyBot <jimmybot@teknik.io>
2017-07-06 09:28:47 -0400

    switched from ruby to lua, void script actually works now, oops :/

---
+++ entropy-up.lua
@@ -0,0 +1,29 @@
+--[[
+    Copyright (C) <2017>  <jimmybot@teknik.io>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
+print("Current entropy")
+os.execute("cat /proc/sys/kernel/random/entropy_avail")
+print("Do you allow me to install haveged for you?")
+print("Enter if yes")
+input = io.read
+print("Installing haveged")
+os.execute("sudo apt install haveged")
+print("haveged installed")
+print("Entropy should be going up")
+print("Check out your entropy now")
+print("Current entropy")
+os.execute("cat /proc/sys/kernel/random/entropy_avail")
