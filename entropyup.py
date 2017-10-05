437a66085c7a012a7fca23bb9b2b95996fbb3310
jmfgdev <jmfgdev@users.noreply.github.com>
2017-10-05 04:01:58 -0400

    Update entropyup.py

--- entropyup.py
+++ entropyup.py
@@ -13,7 +13,7 @@ def entropyup():
     """
 
     """
-        Copyright (C) 2017  jmfgdev@outlook.com
+        Copyright (C) 2017  jmfgdev
         This program is free software: you can redistribute it and/or modify
         it under the terms of the GNU Affero General Public License as published
         by the Free Software Foundation, either version 3 of the License, or
